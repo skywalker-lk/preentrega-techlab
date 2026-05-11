7285504b298096ab75013ce82d7983a34245dcaa
Lucas Acuña <acuna.lucas.m@gmail.com>
2026-05-10 21:26:46 -0300

    Agregar excepciones para armar modulo de Pedido

---
+++ src/exception/PedidoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class PedidoNoEncontradoException extends RuntimeException {
+    public PedidoNoEncontradoException(String message) {
+        super(message);
+    }
+}
